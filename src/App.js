6984760089e7d915b1b3ab14cc2a9515ea02e58b
Zane <nhiphm302@gmail.com>
2023-11-06 23:55:22 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -18,36 +18,6 @@ import Avatar from '@mui/material/Avatar';
 import Paper from '@mui/material/Paper';
 
 function App() {
-  const [user, setUser] = useState(null);
-  const [profile, setProfile] = useState(null);
-  const [userInfoVisible, setUserInfoVisible] = useState(false);
-
-  const login = useGoogleLogin({
-    onSuccess: (codeResponse) => setUser(codeResponse),
-    onError: (error) => console.log('Login Failed:', error)
-  });
-
-  useEffect(() => {
-    if (user) {
-      axios
-        .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-          headers: {
-            Authorization: `Bearer ${user.access_token}`,
-            Accept: 'application/json'
-          }
-        })
-        .then((res) => {
-          setProfile(res.data);
-        })
-        .catch((err) => console.log(err));
-    }
-  }, [user]);
-
-  const logOut = () => {
-    googleLogout();
-    setProfile(null);
-    setUserInfoVisible(false);
-  };
   return (
 
     <div>
@@ -71,40 +41,6 @@ function App() {
 
       </Routes>
 
-      <div className="login-section">
-        {profile ? (
-          <div className="user-info">
-            <Avatar
-              alt="user image"
-              src={profile.picture}
-              onClick={() => {
-                console.log('Avatar clicked');
-                setUserInfoVisible(!userInfoVisible);
-              }}
-            />
-            {userInfoVisible && (
-              <Paper style={{ width: '250px', padding: '16px' }} elevation={3} className="user-info-dropdown">
-                <h4 className="welcome-message">{profile.name}</h4>
-                <p className="email-address">{profile.email}</p>
-                <div style={{textAlign: 'right'}} className="logout-button-container">
-                <button style={{
-                  backgroundColor: 'black', 
-                  color: 'white',
-                  padding: '8px 16px', 
-                  border: 'none', 
-                  cursor: 'pointer' 
-                }} className="logout-button" onClick={logOut}>
-                  Log out
-                </button></div>
-              </Paper>
-            )}
-          </div>
-        ) : (
-          <div className="login-button">
-            <button className="google-login-button" onClick={login}>Sign in with Google 🚀</button>
-          </div>
-        )}
-      </div>
     </div>
 
 
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -10,6 +10,8 @@ import { Link } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
 import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
+import Avatar from '@mui/material/Avatar';
+import Paper from '@mui/material/Paper';
 
 const drawerWidth = 240;
 const navItems = ['Home', 'Dashboard', 'Contact'];
@@ -102,9 +104,49 @@ export default function DrawerAppBar(props) {
                         ))}
                     </Box>
                     {/* Add "Sign in with Google" button here */}
-                    <Button className="google-login-button" onClick={login}>
-                        Sign in with Google 🚀
-                    </Button>
+                    <div className="login-section">
+                        {profile ? (
+                            <div className="user-info">
+                                <Avatar
+                                    alt="user image"
+                                    src={profile.picture}
+                                    onClick={() => {
+                                        console.log('Avatar clicked');
+                                        setUserInfoVisible(!userInfoVisible);
+                                    }}
+                                />
+                                {userInfoVisible && (
+                                    <Paper style={{ width: '250px', padding: '16px' }} elevation={3} className="user-info-dropdown">
+                                        <h4 className="welcome-message">{profile.name}</h4>
+                                        <p className="email-address">{profile.email}</p>
+                                        <div style={{ textAlign: 'right' }} className="logout-button-container">
+                                            <button style={{
+                                                backgroundColor: 'black',
+                                                color: 'white',
+                                                padding: '8px 16px',
+                                                border: 'none',
+                                                cursor: 'pointer'
+                                            }} className="logout-button" onClick={logOut}>
+                                                Log out
+                                            </button></div>
+                                    </Paper>
+                                )}
+                            </div>
+                        ) : (
+                            <div className="login-button">
+                                <Button
+                                    className="google-login-button"
+                                    onClick={login}
+                                    variant="contained"
+                                    color="primary"
+                                    sx={{ color: '#fff' }}
+                                >
+                                    Sign in with Google
+                                </Button>
+                            </div>
+                        )}
+                    </div>
+
                 </Toolbar>
             </AppBar>
             <nav>
@@ -124,6 +166,7 @@ export default function DrawerAppBar(props) {
                     {drawer}
                 </Drawer>
             </nav>
+            
         </Box>
     );
 }
